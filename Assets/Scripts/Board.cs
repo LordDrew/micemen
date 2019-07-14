a0f8855b1b3f72c2a61e2e3a859727a978fc34af
LordDrew <LordDrew@users.noreply.github.com>
2019-07-14 22:38:47 +0300

    Add keyboard controlls

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,6 +6,8 @@ public class Board : MonoBehaviour
 {
     public Column columnPrefab;
     public Column[] columns;
+    public GameObject arrow;
+    int selectedColumn = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,30 @@ public class Board : MonoBehaviour
             col.transform.localPosition = new Vector3(0.75f * i, 0);
             col.name = string.Format("col{0}", i);
         }
+        arrow.transform.localPosition = new Vector3(0, -0.65f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            selectedColumn++;
+            selectedColumn %= 21;
 
+            arrow.transform.localPosition = new Vector3(0.75f * selectedColumn, -0.65f);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            selectedColumn--;
+            if (selectedColumn < 0)
+                selectedColumn = 20;
+
+            arrow.transform.localPosition = new Vector3(0.75f * selectedColumn, -0.65f);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            columns[selectedColumn].MoveUp();
+        }
     }
 }
