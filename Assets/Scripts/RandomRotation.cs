21a0affe40005e35b51b5a698f35724a359a9422
LordDrew <LordDrew@users.noreply.github.com>
2019-07-14 14:16:01 +0300

    Add RandomRotation script

---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(Vector3.forward, 90 * Random.Range(0,3));
+        transform.Rotate(Vector3.up, 180 * Random.Range(0, 2));
+    }
+}
