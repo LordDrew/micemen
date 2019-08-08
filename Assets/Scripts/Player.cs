0778cb423bf86acbf6532ed18fe24388c2d36dca
LordDrew <LordDrew@users.noreply.github.com>
2019-08-09 00:58:53 +0300

    Update simple AI
    Add random column selection

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,12 +12,13 @@ public class Player : MonoBehaviour
     public enum Type
     {
         Human,
-        AlwaysDownAI
+        EasyAI
     }
     public Team team;
     public Type type;
     private Board board;
     private BoardState.TurnState neededTurnState;
+    private bool thinking = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,7 +32,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (board.boardState.turnState != neededTurnState || !board.readyForInput)
+        if (board.boardState.turnState != neededTurnState || !board.readyForInput || thinking)
             return;
 
         switch (type)
@@ -39,8 +40,8 @@ public class Player : MonoBehaviour
             case Type.Human:
                 HumanInput();
                 break;
-            case Type.AlwaysDownAI:
-                AlwaysDownAIInput();
+            case Type.EasyAI:
+                StartCoroutine(EasyAIInput());
                 break;
         }
     }
@@ -57,8 +58,22 @@ public class Player : MonoBehaviour
             board.MoveDown();
     }
 
-    void AlwaysDownAIInput()
+    IEnumerator EasyAIInput()
     {
+        thinking = true;
+        yield return new WaitForSeconds(0.5f);
+        var move = Random.Range(0, board.boardState.validTurns.Count);
+        bool left = move < board.boardState.validTurns.Count / 2f;
+        move /= 2;
+        for (int m = 0; m < move; m++)
+        {
+            if (left)
+                board.MoveLeft();
+            else
+                board.MoveRight();
+            yield return new WaitForSeconds(0.2f);
+        }
         board.MoveDown();
+        thinking = false;
     }
 }
